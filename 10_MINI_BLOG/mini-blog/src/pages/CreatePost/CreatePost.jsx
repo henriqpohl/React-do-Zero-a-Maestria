710b04c0faf7e707df544af0ff4b877ef70f131d
Henrique R. Pohl <henriqpohl@gmail.com>
2023-10-15 17:16:52 +0100

    Finished: Class 142. Validação dos dados.

--- 10_MINI_BLOG/mini-blog/src/pages/CreatePost/CreatePost.jsx
+++ 10_MINI_BLOG/mini-blog/src/pages/CreatePost/CreatePost.jsx
@@ -1,10 +1,124 @@
+// React Hooks
+import { useState } from "react"
+
+// Hooks
+import { useNavigate } from "react-router-dom"
+
+// Custom Hooks
+import { useAuthValue } from "../../contexts/AuthContext"
+import { useInsertDocument } from "../../hooks/useInsertDocument"
+
 // CSS
 import styles from "./CreatePost.module.css"
 
 const CreatePost = () => {
+
+  const [title, setTitle] = useState("")
+  const [image, setImage] = useState("")
+  const [body, setBody] = useState("")
+  const [tags, setTags] = useState([])
+  const [formError, setFormError] = useState("")
+
+  const { user } = useAuthValue()
+
+  const { insertDocument, response } = useInsertDocument("posts")
+
+  const navigate = useNavigate()
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setFormError("")
+
+    // Validate image URL
+    // validate image
+    try {
+      new URL(image)
+    } catch (error) {
+      setFormError("A imagem precisa ser uma URL.")
+    }
+
+    // Criar o arrays de Tags
+    const tagsList = tags.split(",").map((tag) => tag.trim().toLowerCase())
+
+    // Checar todos os valores
+    if (!title || !image || !tags || !body) {
+      setFormError("Por favor, preencha todos os campos!")
+    }
+
+    if (formError) {
+      return
+    }
+
+    insertDocument({
+      title,
+      image,
+      body,
+      tagsList,
+      uid: user.uid,
+      createdBy: user.displayName,
+    })
+
+    // Redirect to Home
+    navigate("/")
+  }
+
+
   return (
-    <div>
-      <h1>CreatePost</h1>
+    <div className={styles.create_post}>
+      <h2>CreatePost</h2>
+      <p>Escreva sobre o que quiser e compartilhe o seu conhecimento!</p>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Título:</span>
+          <input
+            type="text"
+            name="title"
+            required
+            placeholder="Pense em um bom titulo..."
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}
+          />
+        </label>
+        <label>
+          <span>URL da imagem:</span>
+          <input
+            type="text"
+            name="image"
+            required
+            placeholder="Insira uma imagem que represente o seu post..."
+            onChange={(e) => setImage(e.target.value)}
+            value={image}
+          />
+        </label>
+        <label>
+          <span>Conteúdo:</span>
+          <textarea
+            name="body"
+            required
+            placeholder="Insira o conteúdo do seu post..."
+            onChange={(e) => setBody(e.target.value)}
+          />
+        </label>
+        <label>
+          <span>Tags:</span>
+          <input
+            type="text"
+            name="tags"
+            required
+            placeholder="Insira as tags separadas por vírgula..."
+            onChange={(e) => setTags(e.target.value)}
+            value={tags}
+          />
+        </label>
+        {!response.loading && <button className="btn">Cadastrar</button>}
+        {response.loading && (
+          <button className="btn" disabled>
+            Aguarde...
+          </button>
+        )}
+        {response.error && <p className="error">{response.error}</p>}
+        {formError && <p className="error">{formError}</p>}
+      </form>
     </div>
   )
 }
